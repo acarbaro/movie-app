89f068e95dcfd8a4dd1efb9fc1069b4752b45458
Anthony Carbajal <antoncarbaro@gmail.com>
2020-07-02 01:32:54 -0500

    Fix url

--- src/actionCreators/getMovies.js
+++ src/actionCreators/getMovies.js
@@ -6,13 +6,13 @@ export default function getMovies(keyword) {
          dispatch({ type: "SET_LOADING", payload: true });
          let results = [];
          if(keyword && keyword.length){
-            const searchUrl = `${CONSTANTS.SEARCH_URL}/?api_key=${process.env.REACT_APP_API_KEY}&query=${keyword}`;
+            const searchUrl = `${CONSTANTS.SEARCH_URL}?api_key=${process.env.REACT_APP_API_KEY}&query=${keyword}`;
             const searchResponse = await fetch(searchUrl);
             const responseJSON = await searchResponse.json();
             results = responseJSON.results
          } else {
             const sortType = "popularity.desc";
-            const discoverUrl = `${CONSTANTS.DISCOVER_URL}/?api_key=${process.env.REACT_APP_API_KEY}&sort_by=${sortType}`;
+            const discoverUrl = `${CONSTANTS.DISCOVER_URL}?api_key=${process.env.REACT_APP_API_KEY}&sort_by=${sortType}`;
             const discoverResponse = await fetch(discoverUrl);
             const responseJSON = await discoverResponse.json();
             results = responseJSON.results
